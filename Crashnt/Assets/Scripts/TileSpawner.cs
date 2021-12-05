46ac17325752a68173bd91fd714f5e61091a19fb
ascendise <60201207+ascendise@users.noreply.github.com>
2021-12-05 20:43:27 +0100

    Made TileSpawner more readable

--- Crashnt/Assets/Scripts/TileSpawner.cs
+++ Crashnt/Assets/Scripts/TileSpawner.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 public class TileSpawner : MonoBehaviour
 {
 
-    private GameObject tile;
+    private GameObject tilePrefab;
 
+    void Awake()
+    {
+        var tilePath = Path.Combine("Prefabs", "Tile").ToString();
+        tilePrefab = (GameObject)Resources.Load(tilePath); 
+    }
     // Start is called before the first frame update
     void Start()
     {
-        var tilePath = Path.Combine("Prefabs", "Tile").ToString();
-        tile = (GameObject)Resources.Load(tilePath); 
-       for(int z = 1; z <= 11; z++)
-       {
-           var tilePosition = tile.transform.position;
-           var tileWidth = tile.GetComponentInChildren<MeshRenderer>().bounds.size.z;
-           var zPosition = tilePosition.z + (z * tileWidth);
-           var position = tilePosition + new Vector3(0, 0, zPosition);
-           var spawned = Instantiate(tile, Vector3.zero, Quaternion.identity, this.transform);
-           spawned.transform.localPosition = position;
-       }
+        SpawnTiles(10);
+    }
+
+    private void SpawnTiles(int count)
+    {
+        for (int z = 1; z <= count; z++)
+        {
+            var spawned = Instantiate(tilePrefab, Vector3.zero, Quaternion.identity, this.transform);
+            Vector3 position = GetNewTilePosition(z);
+            spawned.transform.localPosition = position;
+        }
+    }
+
+    private Vector3 GetNewTilePosition(int index)
+    {
+        var tilePosition = tilePrefab.transform.position;
+        var tileWidth = tilePrefab.GetComponentInChildren<MeshRenderer>().bounds.size.z;
+        var zPosition = tilePosition.z + (index * tileWidth);
+        var position = tilePosition + new Vector3(0, 0, zPosition);
+        return position;
     }
 
     // Update is called once per frame
