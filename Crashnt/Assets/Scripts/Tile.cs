9fc17fcb8d1921b2dc4b83e991f57ab5c62fd85a
ascendise <rapperido@gmail.com>
2021-12-06 11:44:34 +0100

    Spawn obstacles at random positions

--- Crashnt/Assets/Scripts/Tile.cs
+++ Crashnt/Assets/Scripts/Tile.cs
@@ -1,17 +1,33 @@
 using System;
 using UnityEngine;
-
 public class Tile : MonoBehaviour
 {
     public event EventHandler LeftScreen;
 
+    [SerializeField]
+    private GameObject obstacle;
+    [SerializeField]
+    private GameObject obstacleSpawnArea;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 spawnPosition = GetRandomSpawnPosition();
+        obstacle.transform.position = spawnPosition;
     }
 
-    // Update is called once per frame
+    private Vector3 GetRandomSpawnPosition()
+    {
+        var size = obstacleSpawnArea.GetComponent<Renderer>().bounds.size;
+        var area = obstacleSpawnArea.transform.position;
+        var randomPosition = new Vector3();
+        randomPosition.x = UnityEngine.Random.Range(area.x - size.x / 2, area.x + size.x / 2);
+        randomPosition.y = obstacle.transform.position.y;
+        randomPosition.z = UnityEngine.Random.Range(area.z + size.z / 2, area.z - size.z / 2);
+        return randomPosition;
+    }
+
+    // Update is called once per framelocalPosition
     void Update()
     {
         float speed = -20f;
